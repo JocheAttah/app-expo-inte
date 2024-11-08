f5d48dd38d91b872b38498534741426c94774343
Attah David <jocheattahdavid@gmail.com>
2024-11-08 12:52:00 +0100

    Create index.tsx

---
+++ components/svgs/index.tsx
@@ -0,0 +1,7 @@
+import MorningIcon from './Morning';
+import AfterNoonIcon from './Noon';
+import EveningIcon from './Eve';
+import TwentyFourIcon from './TwentyFour';
+import DateIcon from './Date';
+
+export { MorningIcon, AfterNoonIcon, EveningIcon, TwentyFourIcon, DateIcon };
