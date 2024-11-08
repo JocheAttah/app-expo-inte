51154699c21ae38dcbcb0335e50d6d6ef8b05e6f
Attah David <jocheattahdavid@gmail.com>
2024-11-08 12:50:47 +0100

    Create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Redirect, router } from 'expo-router';
+
+const Index = () => {
+  return <Redirect href="/screens/" />;
+};
+
+export default Index;
+
+const styles = StyleSheet.create({});
