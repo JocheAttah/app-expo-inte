54295161d9eef4bd3c54df810bb30ee5db6b8bfc
Attah David <jocheattahdavid@gmail.com>
2024-11-08 12:52:10 +0100

    Create dummyData.ts

---
+++ constants/dummyData.ts
@@ -0,0 +1,50 @@
+const lagosLGAs = [
+  { id: 1, value: 'Agege' },
+  { id: 2, value: 'Ajeromi-Ifelodun' },
+  { id: 3, value: 'Alimosho' },
+  { id: 4, value: 'Amuwo-Odofin' },
+  { id: 5, value: 'Apapa' },
+  { id: 6, value: 'Badagry' },
+  { id: 7, value: 'Epe' },
+  { id: 8, value: 'Eti-Osa' },
+  { id: 9, value: 'Ibeju-Lekki' },
+  { id: 10, value: 'Ifako-Ijaiye' },
+  { id: 11, value: 'Ikeja' },
+  { id: 12, value: 'Ikorodu' },
+  { id: 13, value: 'Kosofe' },
+  { id: 14, value: 'Lagos Island' },
+  { id: 15, value: 'Lagos Mainland' },
+  { id: 16, value: 'Mushin' },
+  { id: 17, value: 'Ojo' },
+  { id: 18, value: 'Oshodi-Isolo' },
+  { id: 19, value: 'Shomolu' },
+  { id: 20, value: 'Surulere' },
+];
+
+const timeCards = [
+  {
+    id: 1,
+    title: 'Morning',
+    description: 'The time range is',
+    timeRange: '7:00am to 11:59am',
+  },
+  {
+    id: 2,
+    title: 'Afternoon',
+    description: 'The time range is',
+    timeRange: '12:00pm to 4:59pm',
+  },
+  {
+    id: 3,
+    title: 'Evening',
+    description: 'The time range is',
+    timeRange: '5:00pm to 9:59pm',
+  },
+  {
+    id: 4,
+    title: 'All Day',
+    description: 'The time range is',
+    timeRange: '7:00am to 9:59pm',
+  },
+];
+export { lagosLGAs, timeCards };
